40d709ddab222d7739693cd4c7225f533bfe9803
Shirin Sitara <ajsitara1@gmail.com>
2026-02-13 08:07:51 +0000

    Initial Commit

---
+++ JustBeatIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JustBeatIt
+//
+//  Created by Shirin Sitara on 13/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JustBeatIt/JustBeatItApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustBeatItApp.swift
+//  JustBeatIt
+//
+//  Created by Shirin Sitara on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct JustBeatItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
